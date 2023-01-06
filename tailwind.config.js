3620b08d23d692c18af465bbee02c2611d8e28f5
jessicaglz <jessicaglzch@gmail.com>
2023-01-06 09:50:02 -0600

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+   darkMode: false, // or 'media' or 'class'
+   theme: {
+     extend: {
+      fontFamily:{
+        burtons: "burtons",
+      }
+     },
+   },
+   variants: {
+     extend: {},
+   },
+   plugins: [],
+ }
